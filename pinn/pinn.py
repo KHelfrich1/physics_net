45e14e7402ea1e2951bc2ea533e456955440dd94
Kyle Helfrich <khelfrich1@udayton.edu>
2022-09-15 15:19:09 -0400

    Added neural network

--- pinn/pinn.py
+++ pinn/pinn.py
@@ -5,48 +5,43 @@
 # Import modules
 import argparse 
 import os
+import random
 import numpy as np
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
 
-from utils import initial_data
+from utils import training_data, train_network
 
-# import sys
-# #sys.path.insert(0, '../../Utilities/')
+# Set random seeds
+torch.manual_seed(0)
+random.seed(0)
+np.random.seed(0)
 
-# import os
-# import tensorflow as tf
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import scipy.io
-# from scipy.interpolate import griddata
-# from pyDOE import lhs
-# #from plotting import newfig, savefig
-# from mpl_toolkits.mplot3d import Axes3D
-# import time
-# import matplotlib.gridspec as gridspec
-# from mpl_toolkits.axes_grid1 import make_axes_locatable
-
-# import scipy.optimize as sopt
-
-
-# np.random.seed(1234)
-# #tf.set_random_seed(1234)
-# tf.random.set_seed(1234)
-
-# # Turn off eager execution to make compatible with TF2.0+
-# tf.compat.v1.disable_eager_execution()
+# Neural Network
+class Net(nn.Module):
+    
+    def __init__(self, settings):
+        super(Net, self).__init__()
+        self.input_dim = 2
+        self.output_dim = 1
+        self.hidden_sizes = settings.hidden_sizes
+        self.layers = nn.ModuleList()
+        current_dim = self.input_dim
+        for dim in self.hidden_sizes:
+            self.layers.append(nn.Linear(current_dim, dim))
+            current_dim = dim
+        self.layers.append(nn.Linear(current_dim, self.output_dim))
 
 
-# # Used to make compatible with TF2.0+
-# def loss2(values):      
-#     loss_value = tf.reduce_mean(tf.square(values['u0_tf'] - values['u0_pred'])) + \
-#                     tf.reduce_mean(tf.square(values['v0_tf'] - values['v0_pred'])) + \
-#                     tf.reduce_mean(tf.square(values['u_lb_pred'] - values['u_ub_pred'])) + \
-#                     tf.reduce_mean(tf.square(values['v_lb_pred'] - values['v_ub_pred'])) + \
-#                     tf.reduce_mean(tf.square(values['u_x_lb_pred'] - values['u_x_ub_pred'])) + \
-#                     tf.reduce_mean(tf.square(values['v_x_lb_pred'] - values['v_x_ub_pred'])) + \
-#                     tf.reduce_mean(tf.square(values['f_u_pred'])) + \
-#                     tf.reduce_mean(tf.square(values['f_v_pred']))
-#     return loss_value
+    def forward(self, x):
+        for layer in self.layers[:-1]:
+            x = F.relu(layer(x))
+        out = self.layers[-1](x)
+        
+        return out
+        
 
 
 # # Used to print loss values
@@ -66,9 +61,30 @@ from utils import initial_data
 
 #     return
 
-# Main cod component
+
+# Neural Network
+#class Net(nn.Module)
+
+
+
+# Main code component
 def main(settings):
-    x0, u0 = initial_data(settings)
+    
+    # Gather training data
+    U0, Ub, Ui  = training_data(settings)
+
+    # Initialize neural network
+    model = Net(settings)
+
+    # Initialize optimizer object    
+    opt_dict = {'sgd' : optim.SGD(model.parameters(), lr=settings.lr),
+                'adagrad' : optim.Adagrad(model.parameters(), lr=settings.lr),
+                'adam' : optim.Adam(model.parameters(), lr=settings.lr),
+                'rmsprop' : optim.RMSprop(model.parameters(), lr=settings.lr)}
+    opt = opt_dict[settings.optimizer]
+
+    # Train Neural Net
+    train_network(model, U0, Ub, Ui, opt, settings.epochs)
 
 
 # class PhysicsInformedNN:
@@ -338,14 +354,18 @@ if __name__ == "__main__":
                         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
  
     # Loading model parameters
-    parser.add_argument('--hidden_sizes', type=lambda s: [int(item) for item in s.split(',')], default='100,100,100,100,2', help="String of hidden sizes seperated by a comma, for example: '100,100,100,2'")
+    parser.add_argument('--hidden_sizes', type=lambda s: [int(item) for item in s.split(',')], default='100,100,100', help="String of hidden sizes seperated by a comma, for example: '100,100,100,2'")
     parser.add_argument('--optimizer', type=str, default='adam', help='String of optimizer type consisting of [sgd, adam, rmsprop]')
     parser.add_argument('--lr', type=float, default=1e-3, help='Float of the learning rate.')
+    parser.add_argument('--epochs', type=int, default=50000, help='Integer of the number of epochs to train the network.')
     
     # Loading initial conditions
     parser.add_argument('--xL', type=float, default=0, help='Float of the left endpoint of the boundary.')
     parser.add_argument('--xR', type=float, default=1.0, help='Float of the right endpoint of the boundary.')
+    parser.add_argument('--T', type=float, default=2*np.pi, help='Float of the end time.')
     parser.add_argument('--N0', type=int, default=50, help='Integer of the number of datapoints to use for the initial conditions.')
+    parser.add_argument('--Nb', type=int, default=50, help='Integer of the number of datapoints to use for the boundary conditions.')
+    parser.add_argument('--Ni', type=int, default=20000, help='Integer of the number of datapoints to use for the interior points.')
     parser.add_argument('--initial_condition_func', type=str, default='sin', help='String of the initial periodic function consisting of [sin, cos]')
     parser.add_argument('--initial_condition_scaling', type=float, default=1.0, help='Float of the scaling to apply to the periodic intital condition function.')
 
--- pinn/utils.py
+++ pinn/utils.py
@@ -5,23 +5,35 @@ Created on Thu Sep  8 15:31:54 2022
 @author: Kyle Helfrich
 """
 
+import torch
 import numpy as np
 
-def initial_data(settings):
+# Assigning device
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+print('Device: {}'.format(device))
+
+def training_data(settings):
     '''
     Parameters
     ----------
     settings : Object containing the following attributes:
         xL  - Lower limit of x-values
         xR - Right limit of x-values
-        N0 - Number of data points to use for training
+        T- End of time interval
+        N0 - Number of initial condition data points to use for training
+        Nb - Number of boundary data points to use for training
+        Ni - Number of interior data points to use for training
         initial_condition_func - String of which periodic funciton to use
         initial_condition_scaling - Float of the scaling to apply to the periodic initial condition
         
     Returns
     -------
-    X0 - Numpy array of size (N0, 2) of the form (x,t) = (xi, 0)
-    U0 - Numpy array of size (N0, 2) of the form (x, u) = (xi, ui)
+    U0 - Random initial conditions. Numpy array of size (N0, 3) of the form (x,t,u) = (xi, 0, 0)
+    Ub - Random boundary conditions. Numpy array of size (Nb, 3) of the form (x,t,u) = (xi, ti, ui)
+    Ui - Random interior conditions. Numpy array of size (Ni, 3) of the form (x,t,u) = (xi, ti, 0)    
     '''
     
     # Load initial condition function
@@ -30,19 +42,99 @@ def initial_data(settings):
     base_func = init_func[settings.initial_condition_func]
     init_func = lambda x : settings.initial_condition_scaling*base_func(x)
     
-    # Create random samples
+    # Create random initial conditions 
     dx = (settings.xR - settings.xL)/(10*settings.N0)
     x_values = np.arange(settings.xL, settings.xR+dx, dx)
     x_rand = np.random.randint(0, len(x_values), size=(settings.N0))
-    x0 = x_values[x_rand]
+    x0 = np.reshape(x_values[x_rand], (-1,1))
+    U0 = np.concatenate([x0, np.zeros_like(x0), np.zeros_like(x0)], axis=-1)
     
-    # Create initial conditions
-    u0 = init_func(x0)
- 
-    # Create matrics
-    X0 = np.concatenate(((x0,1), (0*x0,1)), axis=1)
-    print(X0.shape, X0[:5,:])
+    # Create random boundary conditions
+    dt = settings.T/(10*settings.Nb)
+    t_values = np.arange(0, settings.T+dt, dt)
+    
+    dTL = int(np.floor(settings.Nb/2.0))
+    dTR = settings.Nb-dTL
+    
+    t_randL = np.random.randint(0, len(t_values), size=(dTL))
+    t_randR = np.random.randint(0, len(t_values), size=(dTR))
+    
+    tL = np.reshape(t_values[t_randL], (-1,1))
+    tR = np.reshape(t_values[t_randR], (-1,1))
+    
+    ub = init_func(tL)
+    UbL = np.concatenate([settings.xL*np.ones_like(tL), tL, ub], axis=-1)
+    UbR = np.concatenate([settings.xR*np.ones_like(tR), tR, np.zeros_like(tR)], axis=-1)
+    Ub =np.concatenate([UbL, UbR], axis=0)
+    
+    
+    # Gather interior points
+    dx = (settings.xR - settings.xL)/(10*settings.Ni)
+    x_values = np.arange(settings.xL, settings.xR+dx, dx)
+    x_rand = np.random.randint(0, len(x_values), size=(settings.Ni))
+    xi = np.reshape(x_values[x_rand], (-1,1))
+    
+    dt = settings.T/(10*settings.Ni)
+    t_values = np.arange(0, settings.T+dt, dt)
+    t_rand = np.random.randint(0, len(t_values), size=(settings.Ni))
+    ti = np.reshape(t_values[t_rand], (-1,1))
+    
+    Ui = np.concatenate([xi, ti, np.zeros_like(xi)]) 
+       
+    return U0, Ub, Ui
+
+###############################################################################
+
+#def losses()
+
+
+###############################################################################
+
+def train_network(model, U0, Ub, Ui, opt, epochs):
+    '''
+    Parameters
+    ----------
+        model : Pytorch neural network object
+        U0 : Initial conditions 
+        Ub : Boundary conditions
+        Ui : Interior point conditions
+        opt : Optimizer
+        epochs : Integer of the number of epochs the model can be run 
+        
+    Returns
+    -------
+    losses : List of loss values
+    model : trained model
+    '''    
+
+    # Convert data types
+    x0 = torch.from_numpy(U0[:,:2]).float().to(device)
+    u0 = torch.from_numpy(U0[:,-1]).float().to(device)
+    
+    temp = []
+    
+    #xi = torch.from_numpy
     
     
     
-    return 1, 2
+    
+
+    # Gather initial results 
+
+
+
+
+      
+    model.to(device)
+    u0_hat = model(torch.from_numpy(U0[:,:2]).float().to(device))
+    print(u0_hat.shape)
+    
+    #u0_hat = model(torch.from_numpy(U0[:,:2]))
+    
+    
+    
+    
+    
+    #for i in range(1, settings.epochs+1):
+        
+    
